090ab61603387efce68262f2be770c1d4be0637d
Patryk Keska <patrykkeska4@gmail.com>
2019-05-11 17:11:23 +0200

    basic layout has been fixed

--- listofcountires/src/components/Item.js
+++ listofcountires/src/components/Item.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "../style/Item.css"
 
 const Item = ({ name, location, population, flag }) => {
 
@@ -8,9 +9,9 @@ const Item = ({ name, location, population, flag }) => {
             <img className="item--img " src={flag} alt={name} />
 
             <div className="item--div-wrapper">
-                <h1>Country : {name}</h1>
-                <h2>Location : {location}</h2>
-                <h2>population : {population}</h2>
+                <h1 className="item--div--h1">Country : {name}</h1>
+                <h2 className="item--div--h2">Location : {location}</h2>
+                <h2 className="item--div--h2">population : {population}</h2>
 
             </div>
         </li>
--- listofcountires/src/components/ListItem.js
+++ listofcountires/src/components/ListItem.js
@@ -2,21 +2,25 @@
 import React from 'react';
 import Item from './Item';
 import data from '../data/data';
+import "../style/ListItems.css"
 
 
 const ListItems = () => {
     return (
-        <ul>
-            {data.map(item => (
-                <Item
-                    key={item.name}
-                    name={item.name}
-                    location={item.location}
-                    population={item.population}
-                    flag={item.flag}
-                />
-            ))}
-        </ul>
+        <>
+            <h1 className="ListItems--h1">List of Countries</h1>
+            <ul className="ListItems--ul">
+                {data.map(item => (
+                    <Item
+                        key={item.name}
+                        name={item.name}
+                        location={item.location}
+                        population={item.population}
+                        flag={item.flag}
+                    />
+                ))}
+            </ul>
+        </>
     )
 }
 
