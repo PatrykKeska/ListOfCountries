bf81afbbe24e9a9eb07a1611efadbf99541b8c6c
Patryk Keska <patrykkeska4@gmail.com>
2019-05-12 10:00:07 +0200

    css modules has been added to project

--- listofcountires/src/components/Input.js
+++ listofcountires/src/components/Input.js
@@ -1,19 +1,19 @@
 
 import React from 'react'
-import '../style/input.css';
+import styles from '../style/input.module.scss';
 
 
 const Input = (props) => {
     return (
-        <div className="input--div--wrapper">
+        <div className={styles.wrapper}>
             <h1>Add Country</h1>
 
-            <form className="input--form" onSubmit={props.handleFnc} >
+            <form className={styles.form__form} onSubmit={props.handleFnc} >
                 <input type="text" placeholder="country name..." />
                 <input type="text" placeholder="location " />
                 <input type="number" placeholder="population in milions " />
                 <input type="text" placeholder="Img link" />
-                <button className="input--form-button" type="submit">Add new Country</button>
+                <button className={styles.form__btn} type="submit">Add new Country</button>
             </form>
 
         </div>
